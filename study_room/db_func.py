d4afef8410cb76e2d83ac14fc9e3a79ebdc1540a
roharon <roharon@hufs.ac.kr>
2018-06-03 01:55:00 +0900

    Initial commit

---
+++ study_room/db_func.py
@@ -0,0 +1,157 @@
+import sqlite3
+import os
+import datetime
+"""
+#make_reservation_db : DB폴더없을때 생성해줌
+#reservation : room DB연결하여 예약생성
+#reserve_main : 
+
+"""
+
+
+def make_reservation_db():
+    """
+    3시간 간격으로 DB 컬럼 채운다.
+
+    start_time | end_time | student | reservation_time 으로 구성되어있으며
+
+    start_time과 end_time은 개발자가 지정(0930~2130), student는 예약자 이름,
+    reservation_text는 사용자 예약당시의 시간이다.
+
+    """
+    path = "./DB"
+    os.makedirs(path, exist_ok=True)
+    os.remove("./DB/room")
+    con = sqlite3.connect('./DB/room')
+
+    cur = con.cursor()
+
+    for i in range(1, 6):
+        cur.execute("CREATE TABLE ROOM" + str(i) + "(start_time text, end_time text, student text, reservation_time text)")
+
+        for j in range(930, 2130, 300):
+            cur.execute("INSERT INTO ROOM" + str(i) + " Values(:start_time, :end_time, :student, :reservation_time);",
+                        {"start_time": j, "end_time": j + 300, "student": '', "reservation_time": ''})
+
+    con.commit()
+    con.close()
+
+
+def reservation(request = None):
+    # NO reservation_time student start_time
+    # 정상이면 0 리턴, 비정상 종료면 1리턴, 기존예약이 있으면 5 리턴
+    con = sqlite3.connect('./DB/room')
+    cur = con.cursor()
+    select = []
+
+    for j in range(1, 6):
+        user_select_room = cur.execute("SELECT start_time FROM ROOM{} WHERE student = {!r}"
+                                       .format(str(j), request['student']))
+        for i in user_select_room.fetchall():
+            select.append(i)
+    print(select)
+    # 유저가 기존에 예약한 스터디룸이 있는가?
+    # 안비어으면 예약안되게 한다.
+    if select != []:
+        con.close()
+        return 5
+
+    now_room = cur.execute("SELECT reservation_time FROM ROOM{0} WHERE start_time = {1};"
+                .format(str(request['NO']), str(request['start_time'])))
+
+    now_room = now_room.fetchall()
+    # now_room 이 비어있으면 UPDATE 없으면 그대로 리턴
+    if now_room == [('',)]:
+        cur.execute("UPDATE ROOM{} SET reservation_time = {}, student = {!r} WHERE start_time = {};"
+                    .format(str(request['NO']), str(request['reservation_time']), str(request['student']),
+                            str(request['start_time'])))
+        # sqlite3 포매팅에 !r을 붙이지않으면 unrecognized token 오류가 난다.
+    else:
+        print(now_room)
+        con.close()
+        return 1
+
+    con.commit()
+    con.close()
+
+    return 0
+
+
+def search_reservation(request = None):
+    con = sqlite3.connect('./DB/room')
+    cur = con.cursor()
+
+    #now_time = datetime.datetime.now()
+    #now_time = int(now_time.strftime("%H%M%S"))
+
+    available_list = {}
+
+    for i in range(1, 6):
+        available_list['ROOM' + str(i)] = []
+        # 스터디룸 갯수별로 딕셔너리에 리스트 타입으로 만들고 append
+        cur.execute("SELECT start_time FROM ROOM{} WHERE reservation_time = '';"
+                    .format(str(i)))
+        # reservation_time 이 비어있으며, 해당 시간대가 현재 시간보다 나중일 경우만 선택
+        # 시간 나중인것은 후에 구현하기로 함. 2018-06-02 21:31
+        for cell in cur.fetchall():
+            available_list['ROOM' + str(i)].append(cell[0])
+            # {'ROOM1': ['1230', '1530', '1830'], 'ROOM2': ['930', '1230', '1530', '1830'], 'ROOM3': ['1530', '1830'],
+            # 'ROOM4': ['930', '1230', '1530', '1830'], 'ROOM5': ['930', '1230', '1530', '1830']}
+
+    con.close()
+    # print(available_list)
+    return available_list
+
+
+    return available_list
+
+def my_reservation(request = None):
+    # student키
+    # 나의 예약현황
+    reservation_list = []
+    #reservation_list 에 [방번호, 사용할 시간, 예약당시 시간]
+
+    con = sqlite3.connect("./DB/room")
+    cur = con.cursor()
+
+    for i in range(1,6):
+        cur.execute("SELECT start_time, reservation_time FROM ROOM{} WHERE student = {!r};"
+                    .format(str(i), request['student']))
+        for j in cur.fetchall():
+            reservation_list.append(['ROOM{}'.format(i), j[0], j[1]])
+    print(reservation_list)
+
+    return reservation_list
+    # [['ROOM1', '930', '110']]
+
+def cancel_reservation(request = None):
+    # 리턴 1은 비정상적 종료,
+    # student키
+    con = sqlite3.connect("./DB/room")
+    cur = con.cursor()
+
+    prev_cancel = my_reservation({"student": request['student']})
+
+    try:
+        cur.execute("UPDATE ROOM{} SET student = '', reservation_time = '' WHERE start_time = {};"
+                    .format(prev_cancel[0][0][4:], prev_cancel[0][1]))
+
+    except Exception as e:
+        con.close()
+        print(e)
+        return "취소할 곳이 없습니다"
+    con.commit()
+    con.close()
+    return "{}번실 {} 시간대 예약 취소되었습니다".format(prev_cancel[0][0][4:], prev_cancel[0][1])
+
+if __name__ == "__main__":
+    # make_reservation_db()
+    #search_reservation()
+    my_reservation({'student': 'roharon'})
+    print("===============")
+    reservation({
+        'NO': '1',
+        'student': 'roharon',
+        'reservation_time': '0110',
+        'start_time': '930',
+    })
---
+++ study_room/views.py
@@ -0,0 +1,196 @@
+from .db_func import make_reservation_db
+from .db_func import reservation
+from .db_func import search_reservation
+from .db_func import cancel_reservation
+from .db_func import my_reservation
+import json
+import os
+import sqlite3
+import datetime
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+def keyboard(request):
+
+    print(request.body)
+    menus = ['예약현황', '예약하기', '예약취소']
+
+    return JsonResponse({
+        'type': "buttons",
+        "buttons": menus,
+    })
+
+@csrf_exempt
+def message(request):
+    menus = ['예약하기', '예약취소', '나의 예약현황']
+    study_room_menu = ['1번실', '2번실', '3번실', '4번실', '5번실', '6번실']
+
+    req = (request.body).decode('utf-8')
+    received_json = json.loads(req)
+
+    content_name = received_json['content']
+    user_name = received_json['user_key']
+
+    now_date = datetime.datetime.now()
+    now_date = now_date.strftime("%H%M")
+    ## 183700 > 164700 + 30000
+    # 18시37분00초
+
+    if not os.path.exists("./DB"):
+        make_reservation_db()  # 생성하게끔
+        # DB폴더없으면
+
+    if content_name == '나의 예약현황':
+        reservation_MyStatus = my_reservation({"student": user_name})
+        # 리턴형식 : [방번호, 사용할 시간, 예약당시 시간]
+        text = "사용자의 예약현황\n"
+        for cell in reservation_MyStatus:
+            if len(cell[1]) == 3:
+                time = "{}시 {}분".format(cell[1][:1], cell[1][1:])
+            else:
+                time = "{}시 {}분".format(cell[1][:2], cell[1][2:])
+
+            text += "{} - {}".format(cell[0], time)
+
+        return JsonResponse({
+            "message": {
+                "text": text
+            },
+            "keyboard": {
+                "type": "buttons",
+                "buttons": menus
+            }
+        })
+
+    elif content_name == '예약하기':
+        # '예약하기' => '번실' => '번실 - '
+        me = ['1번실', '2번실', '3번실', '4번실', '5번실']
+        return JsonResponse({
+            "message": {
+                "text": "스터디룸을 선택해주세요"
+            },
+            "keyboard": {
+                "type": "buttons",
+                "buttons": me
+            }
+        })
+
+    elif '번실 - ' in content_name and len(content_name) < 20:
+        time_sen = str(content_name[6:])
+        time_sen = time_sen.replace('시', '').replace('분','').replace(' ','')
+        reserve_data = {
+            "NO": content_name[:1],
+            "reservation_time": now_date,
+            "student": user_name,
+            "start_time": time_sen,
+        }
+
+        result = reservation(reserve_data)
+        # 정상 0, 비정상종료는 1, 예약한 곳이 있으면 5 리턴
+
+        if result == 0:
+            return JsonResponse({
+                "message": {
+                    "text": "{} 예약되었습니다".format(content_name)
+                },
+                "keyboard": {
+                    "type": "buttons",
+                    "buttons": menus
+                }
+            })
+        elif result == 1:
+            return JsonResponse({
+                "message": {
+                    "text": "예약중 오류가 발생했습니다\n개발자에게 말씀해주세요"
+                },
+                "keyboard": {
+                    "type": "buttons",
+                    "buttons": menus
+                }
+            })
+        elif result == 5:
+            return JsonResponse({
+                "message": {
+                    "text": "이미 예약한 스터디룸이 있습니다\n취소후 예약해주세요"
+                },
+                "keyboard": {
+                    "type": "buttons",
+                    "buttons": menus
+                }
+            })
+
+    elif '번실' in content_name and len(content_name) < 20:
+        available_list = search_reservation()
+        room_no = content_name[:1]
+        can_select = []
+        for can_time in available_list['ROOM' + room_no]:
+            if len(can_time) == 3:
+                can_time = "{}시 {}분".format(can_time[:2], can_time[2:])
+            else:
+                can_time = "{}시 {}분".format(can_time[:2], can_time[2:])
+
+                # can_select는 가능한 호실들 response하기위한 목록
+                # can_time은 available_list 해당호실의 예약가능한 시간들
+
+                can_select.append("{} - {}".format(content_name, can_time))
+                # '2번실 - 9시 30분'
+                # 분류는 '번실 - ' 로 하면된다.
+        #print(can_select)
+        #print('\n\n')
+        print(content_name, menus)
+
+        return JsonResponse({
+            "message": {
+                "text": "예약"
+            },
+            "keyboard": {
+                "type": "buttons",
+                "buttons": can_select
+            }
+        })
+
+    elif '예' == content_name:
+        res = cancel_reservation({"student": user_name})
+        return JsonResponse({
+            "message": {
+                "text": "{}".format(res)
+            },
+            "keyboard": {
+                "type": "buttons",
+                "buttons": menus
+            }
+        })
+
+    elif '아니오' == content_name:
+        return JsonResponse({
+            "message": {
+                "text": "예약취소를 하지않습니다\n메인으로 이동합니다"
+            },
+            "keyboard": {
+                "type": "buttons",
+                "buttons": menus
+            }
+        })
+
+    elif content_name == '예약취소':
+        return JsonResponse({
+            "message": {
+                "text": "예약취소하시겠습니까?"
+            },
+            "keyboard": {
+                "type": "buttons",
+                "buttons": ['예', '아니오']
+            }
+        })
+
+    else:
+        return JsonResponse({
+            "message": {
+                "text": "오류입니다."
+            },
+            "keyboard": {
+                "type": "buttons",
+                "buttons": menus
+            }
+        })
+
